d3b7ff7c65a4ccecb8235ed6a1ea3a75c6e42b21
RjDev <rjdevnic@gmail.com>
2023-01-18 12:59:11 +0530

    add s second security group for wiz auto remedaition issue apprearance

--- security_group.tf
+++ security_group.tf
@@ -1,6 +1,35 @@
 resource "aws_security_group" "wiz_sg_1" {
-  name        = "tf-auto-rem-wiz-test"
-  description = "This security group is an example to test wiz auto remediation"
+  name        = "tf-auto-rem-wiz-test-1"
+  description = "This security group is an example to test wiz auto remediation test one"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    #cidr_blocks = ["49.36.144.231/32"]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    #cidr_blocks = ["49.36.144.231/32"]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_security_group" "wiz_sg_2" {
+  name        = "tf-auto-rem-wiz-test-2"
+  description = "This security group is an example to test wiz auto remediation test two"
 
   ingress {
     from_port   = 80
