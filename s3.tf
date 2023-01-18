b02819d2980b6733665e5fedbc990929f4f4f8bd
RjDev <rjdevnic@gmail.com>
2023-01-18 16:28:29 +0530

    removed s3 and sg

--- s3.tf
+++ s3.tf
@@ -1,8 +1,7 @@
-resource "aws_s3_bucket" "wiz-test-bucket" {
-  bucket = "wiz-cspm-test-bucket-3467098"
-  acl    = "public-read"
-
-  versioning {
-    enabled = false
-  }
-}
+#  bucket = "wiz-cspm-test-bucket-3467098"
+#  acl    = "private"
+#
+#  versioning {
+#    enabled = true
+#  }
+#}
