eb0dae9a3f9acb5666d5a7fbce5182598f83230b
RjDev <rjdevnic@gmail.com>
2023-01-24 12:45:31 +0530

    modify third sg for 8081, 53 and 8080 990 open ports to internet

--- security_group.tf
+++ security_group.tf
@@ -75,6 +75,14 @@ resource "aws_security_group" "wiz_sg_3" {
      cidr_blocks = ["0.0.0.0/0"]
    }
 
+   ingress {
+      from_port   = 990
+      to_port     = 990
+      protocol    = "tcp"
+      #cidr_blocks = ["49.36.144.231/32"]
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
   egress {
     from_port   = 0
     to_port     = 0
